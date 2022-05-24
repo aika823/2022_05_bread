6a64d2930ce2f22d876758a99740bacf336391e5
Starters <chrismin1205@gmail.com>
2022-05-24 23:55:07 +0900

    [front] 퍼블리싱 완료

--- bread/app_main/urls.py
+++ bread/app_main/urls.py
@@ -12,4 +12,17 @@ urlpatterns = [
     path('signup', views.signup),
     path('signin', views.signin),
     path('find_info', views.find_info),
+
+    # 공지사항
+    path('announcement', views.announcement),
+    path('announcement_detail', views.announcement_detail),
+
+    # 1:1문의
+    path('inquiry', views.inquiry),
+    path('inquiry_detail', views.inquiry_detail),
+
+    # 마이페이지
+    path('mypage', views.mypage),
+    path('mypage_order', views.mypage_order),
+    path('mypage_delivery', views.mypage_delivery),
 ]
--- bread/app_main/views.py
+++ bread/app_main/views.py
@@ -21,3 +21,51 @@ def find_info(request):
         'banner_title':'아이디/비밀번호 찾기'
     }
     return render(request, 'find_info.html', context=context)
+
+
+# 공지사항
+def announcement(request):
+    context = {
+        'banner_title':'공지사항'
+    }
+    return render(request, 'announcement.html', context=context)
+
+def announcement_detail(request):
+    context = {
+        'banner_title':'공지사항'
+    }
+    return render(request, 'announcement_detail.html', context=context)
+
+
+# 1:1문의
+def inquiry(request):
+    context = {
+        'banner_title':'1:1문의'
+    }
+    return render(request, 'inquiry.html', context=context)
+
+def inquiry_detail(request):
+    context = {
+        'banner_title':'1:1문의'
+    }
+    return render(request, 'inquiry_detail.html', context=context)
+
+
+# 마이페이지
+def mypage(request):
+    context = {
+        'banner_title':'마이페이지'
+    }
+    return render(request, 'mypage/mypage.html', context=context)
+
+def mypage_order(request):
+    context = {
+        'banner_title':'마이페이지'
+    }
+    return render(request, 'mypage/mypage_order.html', context=context)
+
+def mypage_delivery(request):
+    context = {
+        'banner_title':'마이페이지'
+    }
+    return render(request, 'mypage/mypage_delivery.html', context=context)
